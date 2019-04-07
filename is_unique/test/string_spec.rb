8e646015afc62fbfb09bb1de5cc95afee8e998d1
Robbie Smith <robert.smith160@gmail.com>
2019-04-07 15:23:01 -0600

    Removes duplicate tests

--- is_unique/test/string_spec.rb
+++ is_unique/test/string_spec.rb
@@ -7,27 +7,26 @@ describe String do
 
       expect(string).to respond_to(:is_unique?)
     end
-    context 'when passed a unique string' do
-      it 'returns true' do
-        string = "abcdefg"
-        string.hash_of_character_codes
-
-        expect(string.is_unique?).to be(true)
-      end
-    end
     context 'when simple string' do
       it 'returns true' do
         string = "ab"
         expect(string.is_unique?).to be(true)
       end
 
-      it 'more complex' do
+      it 'returns false' do
+        string = "aa"
+        expect(string.is_unique?).to be(false)
+      end
+    end
+
+    context 'when complex string' do
+      it 'returns true' do
         string = "abeghklzq"
         expect(string.is_unique?).to be(true)
       end
 
       it 'returns false' do
-        string = "aa"
+        string = "aehjfskjdhsfukjh"
         expect(string.is_unique?).to be(false)
       end
     end
