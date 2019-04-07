a3128a45a8ce69490a58ab7faee8de2fd488cf59
Robbie Smith <robert.smith160@gmail.com>
2019-04-07 15:22:28 -0600

    Removes unused code

--- is_unique/lib/string.rb
+++ is_unique/lib/string.rb
@@ -1,15 +1,9 @@
 class String
-  LETTERS = ('a'..'z').to_a + (('A'..'Z').to_a).freeze
-  def hash_of_character_codes
-    LETTERS.each_with_object({}) do |letter, memo|
-      memo[letter] = letter.bytes.pop
-    end
-  end
-
   def is_unique?
     content = {}
     self.chars.each_index do |index|
-      if chars[index] == chars[index + 1]
+      letter = chars[index]
+      if letter == chars[index + 1]
         return false
       elsif content[letter] == true
         return false
