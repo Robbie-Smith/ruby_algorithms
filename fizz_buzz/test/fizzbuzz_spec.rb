68c7882969118da8a7b6ec22e33b858231409559
Robbie Smith <robert.smith160@gmail.com>
2019-04-09 11:41:55 -0600

    Renames variable.

--- fizz_buzz/test/fizzbuzz_spec.rb
+++ fizz_buzz/test/fizzbuzz_spec.rb
@@ -4,22 +4,22 @@ describe Fizzbuzz do
   describe '#buzz' do
     context 'when passed 3' do
       it 'prints fizz' do
-        a = Fizzbuzz.new
-        expect(a.buzz(3)).to eq("fizz")
+        fb = Fizzbuzz.new
+        expect(fb.buzz(3)).to eq("fizz")
       end
     end
 
     context 'when passed 5' do
       it 'prints buzz' do
-        a = Fizzbuzz.new
-        expect(a.buzz(5)).to eq("buzz")
+        fb = Fizzbuzz.new
+        expect(fb.buzz(5)).to eq("buzz")
       end
     end
 
     context 'when passed 15' do
       it 'prints buzz' do
-        a = Fizzbuzz.new
-        expect(a.buzz(15)).to eq("fizz buzz")
+        fb = Fizzbuzz.new
+        expect(fb.buzz(15)).to eq("fizz buzz")
       end
     end
   end
